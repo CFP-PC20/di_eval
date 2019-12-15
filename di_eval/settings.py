60dfd62e91f3a71da1de5f548c970a3f7488e460
José <josejuan.vigil@campusfp.es>
2019-12-15 20:04:11 +0100

    ,...

--- di_eval/settings.py
+++ di_eval/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'di_eval.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
